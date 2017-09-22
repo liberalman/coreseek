42bbeda9a5f3bba360700b920646319c2340d6f6
liberalman <zscchina@163.com>
2017-09-22 16:30:47 +0800

    sphinx ok

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,15 @@ set -e
 
 SPHINX_MODE=${SPHINX_MODE:-}
 
+# get mysql ip
+mysql_ip=`ping mysql -c1 | grep PING | awk '{ print $3 }'|sed 's/[():]//g'`
+# set ip to config
+sed -i "s/sql_host.*/sql_host        = $mysql_ip /g" ${SPHINX_CONF}
+
 if [[ ${SPHINX_MODE} == indexing ]]; then
  indexer --config ${SPHINX_CONF} --all --rotate
- #indexer --config ${SPHINX_CONF} --merge test1 delta --rotate
+elif [[ ${SPHINX_MODE} == merge ]]; then
+ indexer --config ${SPHINX_CONF} --merge test1 delta --rotate
 else
   # allow arguments to be passed to Sphinx search
   if [[ ${1:0:1} = '-' ]]; then
